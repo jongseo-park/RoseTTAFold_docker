9084cb4cc10e5c80e085e5f23fe40ced9d46c46b
jongseo <jongseo@MBP16.local>
2021-08-31 23:42:07 +0900

    Script for copy result files

---
+++ copy_results.py
@@ -0,0 +1,38 @@
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser (description="copy result")
+
+# Arguments
+parser.add_argument("--modelpath", required=False, default="./", help="Path to the directory, model")
+parser.add_argument("--savepath", required=False, default="./picked/", help="Path where the result files are copied")
+
+
+args = parser.parse_args()
+
+
+# Function
+def copy (modelpath = args.modelpath, savepath = args.savepath):
+
+    result = ["model_1.pdb", "model_2.pdb", "model_3.pdb", "model_4.pdb", "model_5.pdb"]
+
+    os.mkdir (savepath)
+
+    with open (f"{savepath}/log.txt", "a") as log:
+        for i in result:
+            j = os.path.realpath(f"{modelpath}/{i}")
+            os.system (f"cp {j} {savepath}/{i}")
+            log.write(i)
+            log.write("-----")
+            log.write(j[48:] + "\n")
+
+
+# Run
+if __name__ == '__main__':
+    copy (
+            modelpath = args.modelpath, 
+            savepath = args.savepath
+    )
+
+    print ("Copy finished")
